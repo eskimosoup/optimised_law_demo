950dfe92ae686bc04c4d0f37f0a7d8338ee15d4c
Optimised server <support@eskimosoup.co.uk>
2015-09-17 11:09:18 +0100

    Fix failing test

--- app/models/award.rb
+++ app/models/award.rb
@@ -9,12 +9,12 @@ class Award < ActiveRecord::Base
   validates :name, :image, presence: true
   validates :award_type, presence: true, inclusion: { in: AWARD_TYPES }
 
-  before_save :smart_add_url_protocol
+  before_save :smart_add_url_protocol, if: Proc.new{|f| f.link.present? }
 
   private
 
     def smart_add_url_protocol
       # http://stackoverflow.com/a/7908693
-      self.link = "http://#{self.link}" unless self.link[/\Ahttp:\/\//] || self.link[/\Ahttps:\/\//]
+      self.link = "http://#{self.link}" unless (self.link[/\Ahttp:\/\//] || self.link[/\Ahttps:\/\//])
     end
 end
