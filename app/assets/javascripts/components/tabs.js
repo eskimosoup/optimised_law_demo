c1bbb065d724a89d79b4d5158cc8ecc1713e9fb5
Optimised server <support@eskimosoup.co.uk>
2015-10-05 13:19:51 +0100

    Snags, layout and functionality amends

--- app/assets/javascripts/components/tabs.js
+++ app/assets/javascripts/components/tabs.js
@@ -7,3 +7,16 @@ $(document).on("click", ".tab-toggle", function(e) {
   $(this).addClass('active');
   $('[data-equalizer-watch]').matchHeight();
 });
+
+$(document).on('click', '.service-tab', function() {
+  var container = $(this).attr('href');
+  var href = $(container + ' .service-overview-read-more').attr('href');
+
+  console.log(href);
+
+  if(href !== undefined) {
+    $('.team-members-link').attr('style', '').attr('href', href + '#team-members-carousel');
+  } else {
+    $('.team-members-link').attr('style', 'display:none;');
+  }
+});
