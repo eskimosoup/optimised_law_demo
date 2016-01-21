0b48e17e12e87f182179054996cc0b52af8e28e6
Optimised server <support@eskimosoup.co.uk>
2016-01-21 16:31:47 +0000

    Activity stream widget

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,7 +6,7 @@ class ServicesController < ApplicationController
     @presented_tour_entries = BaseCollectionPresenter.new(collection: TourEntry.where(page: "#{controller_name.classify}_#{@service.id}").positioned.displayed, view_template: view_context, presenter: TourEntryPresenter)
     @presented_department = DepartmentPresenter.new(object: @service.department, view_template: view_context)
     @presented_service = ServicePresenter.new(object: @service, view_template: view_context)
-    @presented_service_articles = BaseCollectionPresenter.new(collection: @service.articles.limit(3), view_template: view_context, presenter: ArticlePresenter) if @service.articles.present?
+    @presented_service_articles = BaseCollectionPresenter.new(collection: @service.articles.limit(articles_limit), view_template: view_context, presenter: ArticlePresenter) if @service.articles.present?
     @presented_service_testimonials = BaseCollectionPresenter.new(collection: @service.testimonials, view_template: view_context, presenter: TestimonialPresenter) if @service.testimonials.present?
     @presented_service_team_members = BaseCollectionPresenter.new(collection: @service.team_members, view_template: view_context, presenter: TeamMemberPresenter) if @service.team_members.present?
     @presented_offices = BaseCollectionPresenter.new(collection: @service.offices, view_template: view_context, presenter: OfficePresenter) if @service.offices.present?
@@ -34,4 +34,7 @@ class ServicesController < ApplicationController
       @service = Service.displayed.find(params[:id])
     end
 
+    def articles_limit
+      @presented_service.layout != 'widget_focused' ? 3 : 5
+    end
 end
